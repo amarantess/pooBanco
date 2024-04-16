15f3908517119e4e6d9f971212e1caa0505e145b
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-04-15 22:47:54 -0300

    first commit

---
+++ Banco/ContaBancaria.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class ContaBancaria
+    {
+		// Construtor
+        public ContaBancaria()
+        {
+			this.Nome = "";
+			this.Saldo = 0;
+        }
+
+        // Propriedades
+        private string nome;
+
+		public string Nome
+		{
+			get { return nome; }
+			set { nome = value.ToUpper(); }
+		}
+
+		private double conta;
+
+		public double Conta
+		{
+			get { return conta; }
+			set { conta = value; }
+		}
+
+		private double saldo;
+
+		public double Saldo
+		{
+			get { return saldo; }
+			set { saldo = value; }
+		}
+
+		// Métodos
+		public void Deposito()
+		{
+			Deposito:
+			Console.Write("Digite o valor para depósito: ");
+			double valor = double.Parse (Console.ReadLine());
+
+			if (valor > 0)
+			{
+				this.Saldo += valor;
+				Console.WriteLine($"Nome: {this.Nome}");
+				Console.WriteLine($"Conta: {this.Conta}");
+				Console.WriteLine($"Depósito de R${valor} realizado com sucesso");
+				Console.WriteLine($"Novo saldo: R${this.Saldo}");
+			}
+            else
+            {
+				Console.WriteLine("Valor inválido. Tente novamente");
+				Console.WriteLine("Tecle ENTER");
+				Console.ReadLine();
+				goto Deposito;
+            }
+
+        }
+
+		public void Saque()
+		{
+			Saque:
+			Console.Write("Digite o valor de saque: ");
+			double valorSaque = double.Parse (Console.ReadLine());
+
+			if(valorSaque > 0 && valorSaque < this.Saldo)
+			{
+				this.Saldo -= valorSaque;
+                Console.WriteLine($"Nome: {this.Nome}");
+                Console.WriteLine($"Conta: {this.Conta}");
+                Console.WriteLine($"Saque de R${valorSaque} realizado com sucesso");
+                Console.WriteLine($"Novo saldo: R${this.Saldo}");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente ou valor de saque inválido. Tente novamente");
+                Console.WriteLine("Tecle ENTER");
+                Console.ReadLine();
+				goto Saque;
+            }
+        }
+
+	}
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria cb = new ContaBancaria();
+
+            Console.WriteLine("-----------");
+            Console.WriteLine("   BANCO   ");
+            Console.WriteLine("-----------");
+            Console.WriteLine("Tecle Enter");
+            Console.ReadLine();
+            Console.Clear();
+
+        
+            Console.Write("Digite o seu nome: ");
+            cb.Nome = Console.ReadLine();
+            Console.Write("Digite o número da sua conta (apenas números): ");
+            cb.Conta = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Opcao:
+            Console.WriteLine("Escolha as opções abaixo");
+            Console.WriteLine("Opção 1: Depósito");
+            Console.WriteLine("Opção 2: Saque");
+            Console.WriteLine("Qual opção deseja? (1/2)");
+            int opcao = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            switch (opcao)
+            {
+                case 1:
+                    cb.Deposito();
+                    break;
+                case 2:
+                    cb.Saque();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente");
+                    Console.WriteLine("Tecle Enter");
+                    Console.ReadLine();
+                    goto Opcao;
+                    
+
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Deseja outra ação? (Sim ou Não)");
+            string op = Console.ReadLine();
+
+            if (op == "Sim" || op == "sim")
+            {
+                goto Opcao;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
